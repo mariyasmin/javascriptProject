7837863c5ed997371e983e2a43acec5731c81e51
mariyasmin <92326425+mariyasmin@users.noreply.github.com>
2022-01-18 19:51:22 -0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,11 @@
+// Such info must be obtained from Firebase Project, on firebase console
+var firebaseConfig = {
+  apiKey: "AIzaSyBbsKGScKePCNLYanjIsKsegXr6AQsO-Oo",
+  authDomain: "todo-list-3c59b.firebaseapp.com",
+  projectId: "todo-list-3c59b",
+  storageBucket: "todo-list-3c59b.appspot.com",
+  messagingSenderId: "559382852890",
+  appId: "1:559382852890:web:8125776561ef735b6928a3",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
